d142f81f0c5aa61936a5209e1fae85c375ba3138
A Senna <ddi0168@gmail.com>
2017-04-15 04:58:55 -0400

    First commit - adding sprites and basic player movement

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	void Awake() {
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Destroying gameObject");
+		}
+		else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+}
